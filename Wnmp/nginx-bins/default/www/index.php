3a58c7b7459280cdd5ed2b238ada53b3e92749fa
Dingjerry <dingjian@foxmail.com>
2025-02-09 23:00:16 +0800

    1.升级到PHP8.4.5，PHP.ini中禁用了imap扩展 2.升级到MariaDB 11.7.2 3.升级后phpMyAdmin会报错，就增加了Adminer5.1.1给Localhost增加了快捷键功能

--- Wnmp/nginx-bins/default/www/index.php
+++ Wnmp/nginx-bins/default/www/index.php
@@ -3,10 +3,11 @@
 <title>Welcome to Wnmp!</title>
 </head>
 <body>
-<center><h2>Welcome to Wnmp 4.2.0!</h2></center>
+<center><h2>Welcome to Wnmp 4.2.1!</h2></center>
 <center><h2><a href="https://wnmp.x64architecture.com/">Wnmp Website</a></h2></center>
 <center><h2><a href="https://wnmp.x64architecture.com/donate">Donate</a></h2></center>
-<center>Click <a href="http://localhost/phpmyadmin/">here</a> to go to phpMyAdmin</center>
+<center>Click <a href="http://localhost/adminer/">here</a> to go to Adminer</center>
+<center>Click <a href="http://localhost/phpmyAdmin/">here</a> to go to phpmyAdmin</center>
 <br />
 <?php 
 phpinfo();
